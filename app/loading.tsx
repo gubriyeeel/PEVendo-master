85e4ec8343ea89501ba3cbd6d46c2fed5f88f02b
gubriyeeel <officialgabrielsufrir@gmail.com>
2024-11-13 23:34:48 +0800

    feat: edit loading to intiate deployment

--- app/loading.tsx
+++ app/loading.tsx
@@ -1,7 +1,9 @@
+import { Loader2 } from "lucide-react";
+
 const loading = async () => {
   return (
     <div className="flex h-[100vh] w-full items-center justify-center text-5xl">
-      Loading...
+      <Loader2 className="animate-spin" />
     </div>
   );
 };
