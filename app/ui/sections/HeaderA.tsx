5ca45ca9702b523ff419e13e12711d210ec04e88
gubriyeeel <officialgabrielsufrir@gmail.com>
2024-11-13 23:56:51 +0800

    remove header menu list

--- app/ui/sections/HeaderA.tsx
+++ app/ui/sections/HeaderA.tsx
@@ -1,24 +1,14 @@
-// Header when no account is logged in
-
 "use client";
 
-import React, { useEffect } from "react";
-import { signOut } from "@/app/lib/authactions";
 import { useState } from "react";
 import Hamburger from "hamburger-react";
-import Link from "next/link";
 
 const HeaderA = () => {
   const [open, setOpen] = useState(false);
 
-  const links = [
-    { name: "Home", link: "/" },
-    { name: "About", link: "/about" },
-  ];
-
   return (
     //
-    <div className="bg-reddish-100 flex w-full flex-col items-center justify-center overflow-hidden shadow-md md:h-[15vh]">
+    <div className="flex w-full flex-col items-center justify-center overflow-hidden bg-reddish-100 shadow-md md:h-[15vh]">
       {/* Main */}
       <div className="flex w-full items-center justify-between px-3 py-1 md:w-11/12 md:py-4">
         {/* Logo  */}
@@ -27,17 +17,6 @@ const HeaderA = () => {
           <div className="flex aspect-square items-center justify-center rounded-full border-2 border-black bg-white p-2">
             Logo
           </div>
-          <ul
-            className={`hidden w-max items-center justify-center gap-x-9 overflow-hidden md:flex md:w-auto md:justify-evenly ${open ? "py-3" : "p-0"}`}
-          >
-            {links.map((item) => {
-              return (
-                <Link key={item.name} href={item.link}>
-                  <li className="text-xl text-white">{item.name}</li>
-                </Link>
-              );
-            })}
-          </ul>{" "}
         </div>
 
         {/* Hamburger button */}
@@ -52,29 +31,6 @@ const HeaderA = () => {
             toggle={setOpen}
           />
         </div>
-      </div>
-
-      {/* Dropdown Menu */}
-      <div
-        className={`bg-Maroon grid w-full overflow-hidden transition-all duration-300 ease-in-out ${open ? `grid-rows-[1fr] opacity-100` : `grid-rows-[0fr] opacity-0`}`}
-      >
-        <ul
-          className={`bg-Maroon flex w-full flex-col items-center justify-center gap-y-2 overflow-hidden ${open ? "py-3" : "p-0"}`}
-        >
-          {links.map((item) => {
-            return (
-              <Link
-                key={item.link}
-                href={item.link}
-                onClick={() => {
-                  setOpen(false);
-                }}
-              >
-                <li className="py-1 text-2xl text-white">{item.name}</li>
-              </Link>
-            );
-          })}
-        </ul>
       </div>
     </div>
   );
